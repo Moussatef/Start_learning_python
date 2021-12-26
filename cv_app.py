2000c77e40ef36537e5c0a17ffbef79df7e4e951
Moussatef <otman.moussetaf@gmail.com>
2021-12-26 23:21:29 +0100

    py

--- cv_app.py
+++ cv_app.py
@@ -2,13 +2,18 @@ from docx import Document
 from docx.shared import Inches
 import pyttsx3
 
-pyttsx3.speak("OTHMAN")
+
+def speak(text):
+    pyttsx3.speak(text)
+
 
 docx = Document()
 
 
 class Person:
-    def __init__(self, first_name, last_name, age, gender, phone, address, mail):
+
+    def __init__(self, first_name, last_name, age, gender, phone, address,
+                 mail):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -18,10 +23,18 @@ class Person:
         self.mail = mail
 
 
+speak("What is your First Name")
 first_name = input("What is your First Name : ")
+speak("Helo " + first_name + " can you give me your last name")
 last_name = input("What is your Last Name : ")
+speak("and what is your email address")
 mail = input("What is your Email : ")
+speak("Please give your age ")
 age = input("What is your Age : ")
+
+speak("Your complet name is " + first_name + " " + last_name +
+      " your email is " + mail + " your age is " + age)
+
 while int(age) < 0 and int(age) > 80:
     print("age invalid")
     age = input("What is your age : ")
@@ -38,13 +51,11 @@ person = Person(first_name, first_name, age, gender, phone, address, mail)
 
 docx.add_picture("pictures/QTR11058.jpg", width=Inches(1.4))
 docx.add_heading("Personal Information ")
-docx.add_paragraph(
-    person.first_name.upper() + " " + person.last_name.upper()
-).bold = True
+docx.add_paragraph(person.first_name.upper() + " " +
+                   person.last_name.upper()).bold = True
 docx.add_paragraph("Adrress : " + person.address)
 docx.add_paragraph("Age : " + str(person.age))
 docx.add_paragraph("Gender : " + person.gender)
-
 
 docx.add_heading("Contact Information")
 contact = "Email : " + person.mail + "\nPhone : " + person.phone
@@ -71,6 +82,5 @@ while True:
         continue
     else:
         break
-
 
 docx.save("import_result/cv_1.docx")
